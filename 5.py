fc5fcb82e5cb61fe46f49611a77888da48a50a7d
Ro <757306650@qq.com>
2014-09-03 01:11:14 +0800

    1

---
+++ 5.py
@@ -0,0 +1,51 @@
+# -- coding: utf-8 --
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is trick, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+
+
+print "\n"
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is trick, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+
+Hello_World = "Hello World!"
+print "%r" %Hello_World
+
+a = 1
+print "%f" %a
+
+print "输入英寸"
+x_in = input()
+x_cm = x_in * 2.54
+print "%f英寸等于%f厘米" %(x_in,x_cm)
